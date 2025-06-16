5189ef1b134cbca2d53107b7069a5127571ffe88
Zane Wang <140219691+zanewnch@users.noreply.github.com>
2025-06-16 14:06:47 +0800

    delete the unnecessary scripts.

--- AIOT/backend/dist/config/mongodb.d.ts
+++
@@ -1,4 +0,0 @@
-import mongoose from 'mongoose';
-export declare function connectToMongoDB(): Promise<void>;
-export default mongoose;
-//# sourceMappingURL=mongodb.d.ts.map
--- AIOT/backend/dist/config/mysql.d.ts
+++
@@ -1,7 +0,0 @@
-import mysql from 'mysql2/promise';
-declare const pool: mysql.Pool;
-export declare function connectToMySQL(): Promise<void>;
-export declare function getPool(): mysql.Pool;
-export { pool };
-export default pool;
-//# sourceMappingURL=mysql.d.ts.map
